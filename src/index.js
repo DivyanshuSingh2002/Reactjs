9d95d443bb4bf52ead98a8d82c23ca9ba193601f
Divyanshu Singh <singhdivyanshu854@gmail.com>
2023-01-13 11:29:21 +0530

    message

---
+++ src/index.js
@@ -0,0 +1,15 @@
+let a = 10;
+if(a%2==0){
+    document.write(a+" is a even number." +"<br\>");
+}
+else{
+    document.write(a+" is odd number." + "<br\>");
+}
+
+//Hosting in js
+document.write(x + "<br\>");
+greeting();
+var x = 10;
+function greeting(){
+    document.write("Good Morning!");
+}
